306d1be1aa267afca2e3e71944bb464c08ec50f2
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:45:41 +0100

    correction

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <body>
     <p>
       <?php
+      // si l'âge est supérieur ou égal à 18, tu es majeur, sinon tu n'es pas majeur
         echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur';
       ?>
     </p>
@@ -16,9 +17,16 @@
         if ($age >= 18){
           echo 'Tu es majeur';
         }else{
-          echo 'Tu es mineur';
+          echo 'Tu n\'es pas majeur';
         }
       ?>
     </p>
+    <!-- Si tu as 18 ans ou plus tu es majeur. -->
+    <?php if ($age >= 18){ ?>
+      <p>Tu es majeur</p>
+      <!-- Sinon tu n'es pas majeur. -->
+    <?php } else { ?>
+      <p>Tu n'es pas majeur</p>
+    <?php } ?>
   </body>
 </html>
